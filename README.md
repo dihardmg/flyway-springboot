47294f5f9757c65ebf74091d21096bd124dee9d9
Didik Hariyanto <com.didikhariyanto@gmail.com>
2017-10-05 21:07:26 +0700

    springboot flyway sample

---
+++ README.md
@@ -0,0 +1,1 @@
+Springboot Flyway Sample
---
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO  peserta(id,nama,email,tanggal_lahir)
+VALUE ('aa1','joni','joni@gmail.com','2012-1-1');
+INSERT INTO  peserta(id,nama,email,tanggal_lahir)
+VALUE ('aa2','paijo','paijo@gmail.com','2012-1-1');
+INSERT INTO  peserta(id,nama,email,tanggal_lahir)
+VALUE ('aa3','pajan','pajan@gmail.com','2012-1-1');
