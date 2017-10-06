ff67c5e317ca2547f5cfd7553b7f4789bf12f02a
Didik Hariyanto <com.didikhariyanto@gmail.com>
2017-10-06 22:35:58 +0700

    1thmwqweax

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -2,8 +2,8 @@ INSERT INTO peserta VALUES
   ('aa1','peserta.test.001','peserta.test.001@gmail.com','2011-01-01'),
   ('aa2','peserta.test.002','peserta.test.002@gmail.com','2012-02-02'),
   ('aa3','peserta.test.003','peserta.test.003@gmail.com','2013-03-03'),
-  ('aa4','peserta.test.004','peserta.test.004@gmail.com','2014-02-02'),
-  ('aa5','peserta.test.005','peserta.test.006@gmail.com','2015-02-02');
+  ('aa4','peserta.test.004','peserta.test.004@gmail.com','2014-04-02'),
+  ('aa5','peserta.test.005','peserta.test.006@gmail.com','2015-05-02');
 
 
 
