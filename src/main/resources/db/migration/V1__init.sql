3202db30a4cbb137f3ec011af75933a520979595
Didik Hariyanto <com.didikhariyanto@gmail.com>
2017-10-06 22:00:04 +0700

    1thmw

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,6 @@
-CREATE TABLE `peserta` (
-  `id` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `nama` varchar(150) NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_4pf6h2ver6d4igrrq6vvo4dbv` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE peserta (
+  id varchar(255) PRIMARY KEY NOT NULL,
+  email varchar(255) NOT NULL UNIQUE,
+  nama varchar(150) NOT NULL,
+  tanggal_lahir date NOT NULL
+);
